dd2007f643b2de1c94a417a3ea4fec379a562793
Iosif-Claudiu Ceaca <ceacaiosifclaudiu@gmail.com>
2023-10-30 18:04:07 +0200

    Changing fetching url

--- client/src/navigation/actions/fetchContacts.js
+++ client/src/navigation/actions/fetchContacts.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:5000"; //I let the url here for testing purpose
+const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 export const fetchContacts = async () => {
   try {
--- client/src/navigation/loaders/contact.js
+++ client/src/navigation/loaders/contact.js
@@ -1,7 +1,7 @@
 import { redirect } from "react-router-dom";
 import { fetchData } from "../../navigation/actions/fetchContacts";
 
-const BASE_URL = "http://localhost:5000"; //I let the url here for testing purpose
+const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 export const contactEditLoader = async ({ params, request }) => {
   let formData = await request.formData();
